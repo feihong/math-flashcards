9f1a51d69818f082fed09b0ec26fb12c322e01f5
Feihong Hsu <feihong.hsu@gmail.com>
2023-10-01 10:38:34 -0500

    Playground now converts from AsciiMath to TeX

--- publish.py
+++ publish.py
@@ -2,7 +2,11 @@ import subprocess
 import shutil
 from pathlib import Path
 
+src_dir = Path('src')
 content_dir = Path('content')
+
+js_file = 'ASCIIMathTeXImg.js'
+shutil.copy(src_dir / js_file, content_dir / js_file)
 
 cmd = [
   'ghp-import',
--- src/convert-asciimath.js
+++ src/convert-asciimath.js
@@ -1,9 +1,10 @@
 {
+  // Convert AsciiMath chunks to TeX chunks
   const convert = (str) => {
     return str
-      .replace(/`(.*?)`/g, (_match, group1) => '\\(' + window.AMTparseAMtoTeX(group1) + '\\)')
-      .replace(/&lt;asciimath&gt;(.*?)&lt;\/asciimath&gt;/g, (_match, group1) => '\\[' + window.AMTparseAMtoTeX(group1) + '\\]')
-  };
+      .replace(/```([^`]+)```/g, (_match, group1) => '\\[' + window.AMTparseAMtoTeX(group1) + '\\]')
+      .replace(/`([^`]+)`/g, (_match, group1) => '\\(' + window.AMTparseAMtoTeX(group1) + '\\)')
+  }
 
   for (const cardEl of document.querySelectorAll('.math-1f960074-6883-4e97-b1bd-1bbfd36be0cb')) {
     cardEl.innerHTML = convert(cardEl.innerHTML);
